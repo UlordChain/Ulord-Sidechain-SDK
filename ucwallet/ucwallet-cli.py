dbae7a283e0c4ca9ab28c9022fcab6010fb47e85
CAOLINAN <1325380061@qq.com>
2018-07-19 14:11:48 +0800

    update udfs module

--- ucwallet/ucwallet-cli.py
+++ ucwallet/ucwallet-cli.py
@@ -17,7 +17,7 @@ from prompt_toolkit.completion import WordCompleter  # 词汇建议
 sys.path.insert(0, os.path.dirname(os.getcwd()))
 from ucwallet import __version__
 from ucwallet.content_contract import ContentContract
-from ucwallet.udfs.udfs import UdfsHelper
+from ucwallet.udfs.udfs import Udfs
 from ucwallet.version import PACKAGE_ROOT
 
 
@@ -34,58 +34,21 @@ class UCwallet():
             keystore_pwd=keystore_pwd,
             **kwargs
         )
-        self.udfs_helper = UdfsHelper()
-        self.load()
-
-    def set_private_key(self, keystorefile, keystore_pwd):
-        """重新加载私钥文件和密码"""
-        return self.content_contract.set_private_key(keystorefile=keystorefile, keystore_pwd=keystore_pwd)
-
-    def transfer_token(self, to_address, value, sum=0):
-        """交易token"""
-        return self.content_contract.transfer_token(to_address=to_address, value=value, sum=sum)
-
-    def publish_resource(self, udfs_hash, author_address, price, deposit, t=1):
-        """发布资源"""
-        return self.content_contract.publish_resource(udfs_hash=udfs_hash, author_address=author_address, price=price, deposit=deposit, t=t)
-
-    def transfer_tokens(self, addresses, qualitys):
-        """多地址结算"""
-        addresses = addresses.split(',')
-        return self.content_contract.transfer_tokens(addresses=addresses, qualitys=qualitys)
-
-    def get_for_receipt(self, tx_hash):
-        """获取交易回执"""
-        return self.content_contract.get_for_receipt(tx_hash=tx_hash)
-
-    def get_gas_balance(self):
-        """获取侧链余额"""
-        return self.content_contract.get_gas_balance()
-
-    def get_token_balance(self, address=None):
-        """获取token余额"""
-        return self.content_contract.get_token_balance(address)
-
-    def transfer_gas(self, to_address, value):
-        """交易gas"""
-        return self.transfer_gas(to_address=to_address, value=value)
+        self.udfs_helper = Udfs()
+        self._load()
 
     def _get_commands(self):
         """get current commands"""
-        # self.CONTRACT_COMMANDS = [command[0] for command in inspect.getmembers(self.content_contract, predicate=inspect.ismethod) if not command[0].startswith('_')]
-        # self.CLI_COMMANDS = [command[0] for command in inspect.getmembers(self, predicate=inspect.ismethod) if not command[0].startswith('_')]
-        # self.UDFS_COMMANDS = [command[0] for command in inspect.getmembers(self.udfs_helper, predicate=inspect.ismethod) if not command[0].startswith('_')]
-        # self.BASIC_COMMANDS = self.CONTRACT_COMMANDS + self.CLI_COMMANDS + self.UDFS_COMMANDS
         self.BASIC_COMMANDS = [command[0] for command in inspect.getmembers(self, predicate=inspect.ismethod) if not command[0].startswith('_')]
 
-    def load(self, logfile=os.path.join(PACKAGE_ROOT,'ucwallet.log'), history='history.txt'):
+    def _load(self, logfile=os.path.join(PACKAGE_ROOT,'ucwallet.log')):
         """加载cli一些必要的配置文件"""
         if self.logfile:
             self.logfile =logfile
         else:
             self.logfile = os.path.join(PACKAGE_ROOT,'ucwallet.log')
         self.log = logging.getLogger('ucwallet')
-        self.history = history
+        # self.history = 'history.txt'
         # setting log module todo set log max file
         logging.basicConfig(
             level='INFO',
@@ -124,15 +87,6 @@ class UCwallet():
             bold=True
         ))
 
-    # command
-    def help(self, *args):
-        """打印帮助信息"""
-        result = ''
-        for command in self.BASIC_COMMANDS:
-            method_to_call = getattr(self, command)
-            result += command.ljust(20) + "\t" + method_to_call.__doc__ + "\n"
-        return result
-
     def _run_cli(self):
         """交互式shell运行"""
         try:
@@ -165,6 +119,57 @@ class UCwallet():
             self.log.error("Error do while:{}".format(e))
             self._info('Goodbye!')
 
+    # command
+    def help(self, *args):
+        """打印帮助信息"""
+        result = ''
+        for command in self.BASIC_COMMANDS:
+            method_to_call = getattr(self, command)
+            result += command.ljust(20) + "\t" + method_to_call.__doc__.split(':')[0].strip() + "\n"
+        return result
+
+    def set_private_key(self, keystorefile, keystore_pwd):
+        """重新加载私钥文件和密码"""
+        return self.content_contract.set_private_key(keystorefile=keystorefile, keystore_pwd=keystore_pwd)
+
+    def transfer_token(self, to_address, value, sum=0):
+        """交易token"""
+        return self.content_contract.transfer_token(to_address=to_address, value=value, sum=sum)
+
+    def publish_resource(self, udfs_hash, author_address, price, deposit, t=1):
+        """发布资源"""
+        return self.content_contract.publish_resource(udfs_hash=udfs_hash, author_address=author_address, price=price, deposit=deposit, t=t)
+
+    def transfer_tokens(self, addresses, qualitys):
+        """多地址结算"""
+        addresses = addresses.split(',')
+        return self.content_contract.transfer_tokens(addresses=addresses, qualitys=qualitys)
+
+    def get_for_receipt(self, tx_hash):
+        """获取交易回执"""
+        return self.content_contract.get_for_receipt(tx_hash=tx_hash)
+
+    def get_gas_balance(self):
+        """获取侧链余额"""
+        return self.content_contract.get_gas_balance()
+
+    def get_token_balance(self, address=None):
+        """获取token余额"""
+        return self.content_contract.get_token_balance(address)
+
+    def transfer_gas(self, to_address, value):
+        """交易gas"""
+        return self.transfer_gas(to_address=to_address, value=value)
+
+    def upload(self, file_path):
+        """
+        上传文件获取hash值
+
+        :param file_path: 文件路径（绝对路径）
+        :return: udfs哈希值
+        """
+        return self.udfs_helper.upload(file_path)
+
 
 @click.command()
 @click.help_option('-h', '--help')
@@ -185,7 +190,8 @@ def cli(keystorefile, keystore_pwd, logfile):
         sys.exit(-1)
     # 初始化CLI
     ucwallet = UCwallet(keystorefile=keystorefile, keystore_pwd=keystore_pwd)
-    ucwallet.load(logfile=logfile)
+    if logfile:
+        ucwallet._load(logfile=logfile)
     ucwallet._run_cli()
 
 
